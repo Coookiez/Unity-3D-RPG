339ef00286667669864b004bd0d0c2ef5d954f00
Coookiez <insanecookiez22@gmail.com>
2017-07-06 02:33:59 +0200

    Movement bug fixes

---
+++ RPG 3D/Dragon RPG/Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+    [SerializeField] Texture2D enemyCursor = null;
+
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+    CameraRaycaster cameraRaycaster;
+
+    // Use this for initialization
+    void Start() {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+
+        switch (cameraRaycaster.currentLayerHit) {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(enemyCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("Don't know what cursor to show.");
+                return;
+        }
+    }
+}
